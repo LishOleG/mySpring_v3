7a04ca6f40c0cd05f2c2f395150c1f0c84ac4bd0
LishOleG <lisholeg16@gmail.com>
2024-05-22 23:11:12 +0500

    Task finish/Решение завершено

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,26 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class );
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class);
+
+
+        System.out.println("Переменные ссылаются на один и тот же объект?" +
+                (bean1 == bean2));
+        System.out.println(bean1.getMessage());
+        System.out.println(bean2.getMessage());
+
+
+
+        Cat myCat1 = (Cat) applicationContext.getBean("cat", Cat.class);
+        Cat myCat2 = (Cat) applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Переменные ссылаются на один и тот же объект?" +
+                (myCat1 == myCat2));
+
+        System.out.println(myCat1.getName());
+        System.out.println(myCat2.getName());
     }
 }
